f4613fc1d73571cc34c660e26e5140f5d4ab1e31
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-07-11 18:23:30 -0400

    Navbar transition complete.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -29,6 +30,7 @@ export function HttpLoaderFactory(http: HttpClient) {
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     HttpClientModule,
     TranslateModule.forRoot({
--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,14 +1,64 @@
-import { Component } from '@angular/core';
-
+import { Component, AfterViewInit, HostBinding } from '@angular/core';
+import {
+  distinctUntilChanged,
+  filter,
+  map,
+  pairwise,
+  share,
+  throttleTime,
+  tap
+} from 'rxjs/operators';
+import {
+  animate,
+  state,
+  style,
+  transition,
+  trigger
+} from '@angular/animations';
+import { fromEvent } from 'rxjs';
+import { VisibilityState } from './header.models'
 // Translator
 import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
+  animations: [
+    trigger('toggle', [
+      state(
+        VisibilityState.Hidden,
+        style({ background: 'transparent', color: 'white' })
+      ),
+      state(
+        VisibilityState.Visible,
+        style({ background: 'white', color: 'green' })
+      ),
+      transition('* => *', animate('200ms ease-in'))
+    ])
+  ]
 })
-export class HeaderComponent {
+export class HeaderComponent implements AfterViewInit {
+
+  private isVisible = false;
+
+  @HostBinding('@toggle')
+  get toggle(): VisibilityState {
+    return this.isVisible ? VisibilityState.Visible : VisibilityState.Hidden;
+  }
+
+  ngAfterViewInit() {
+    const scroll$ = fromEvent(window, 'scroll').pipe(
+      throttleTime(10),
+      map(() => window.pageYOffset > 0 ? VisibilityState.Visible : VisibilityState.Hidden),
+      distinctUntilChanged(),
+      share(),
+      tap((v) => console.log('scroll', v))
+    );
+
+    // Can this be done better?
+    scroll$.subscribe((visibility: VisibilityState) => visibility === 'visible' ? this.isVisible = true : this.isVisible = false)
+  }
 
   constructor(private translate: TranslateService) {
     translate.setDefaultLang('en');
@@ -18,5 +68,8 @@ export class HeaderComponent {
     this.translate.use(language);
   }
 
+  ngOnDestroy(): void {
+    console.log('Header shouldn`t be destroyed');
+  }
 
 }
