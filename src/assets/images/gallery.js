b8e9d9e114e81a61b3cd90337a7d8a866a71ba18
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-09-11 19:51:05 -0400

    Team section.

--- src/assets/images/gallery.js
+++ src/assets/images/gallery.js
@@ -1,27 +1,36 @@
+import { douglas } from '../images/team-douglas.jpg';
+import { ken } from '../images/team-ken.jpg';
+import { tim } from '../images/team-tim.jpg';
+import { amado } from '../images/team-amado.jpg';
+import { juan } from '../images/team-juan.jpg';
+import { wilfredo } from '../images/team-wilfredo.jpg';
+import { vivian } from '../images/team-vivian.jpg';
+
+export const team = [douglas, ken, tim, amado, juan, wilfredo, vivian];
+
 export const gallery1 = [
   'https://i.imgur.com/HAoalIc.jpg',
+  'https://i.imgur.com/MaISdFB.jpg',
   'https://i.imgur.com/ZEup2df.jpg',
   'https://i.imgur.com/fs8AeSF.jpg',
   'https://i.imgur.com/YNKV7dU.jpg',
   'https://i.imgur.com/WpzbXcb.jpg',
   'https://i.imgur.com/ri3CoGZ.jpg',
   'https://i.imgur.com/Zy0RLVC.jpg',
-  'https://i.imgur.com/codpvXG.jpg',
-  'https://i.imgur.com/Ui7D59h.jpg',
   'https://i.imgur.com/piI92fA.jpg'
 ];
 
 export const gallery2 = [
   'https://i.imgur.com/BWoomUu.jpg',
   'https://i.imgur.com/jI928SN.jpg',
+  'https://i.imgur.com/iwpSzd1.jpg',
   'https://i.imgur.com/ub4XC6m.jpg',
   'https://i.imgur.com/WSf6Ghr.jpg',
   'https://i.imgur.com/3vZbRwO.jpg',
   'https://i.imgur.com/EBBGyE3.jpg',
   'https://i.imgur.com/A4XkAUc.jpg',
   'https://i.imgur.com/W9g0iSF.jpg',
-  'https://i.imgur.com/ZB6ooSt.jpg',
-  'https://i.imgur.com/iwpSzd1.jpg'
+  'https://i.imgur.com/ZB6ooSt.jpg'
 ];
 
 export const gallery3 = [
@@ -47,5 +56,6 @@ export const gallery4 = [
   'https://i.imgur.com/NrhpCcp.jpg',
   'https://i.imgur.com/O6Qybxx.jpg',
   'https://i.imgur.com/hPd1t5p.jpg',
-  'https://i.imgur.com/MaISdFB.jpg'
+  'https://i.imgur.com/codpvXG.jpg',
+  'https://i.imgur.com/Ui7D59h.jpg'
 ];
