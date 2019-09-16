f426f139c19c71a756353b0ce39e5005742b02cf
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-09-16 19:26:47 -0400

    Parallax splitters

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import simpleParallax from 'simple-parallax-js';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   scrollToElement(element: HTMLElement): void {
     element.scrollIntoView({
       behavior: 'smooth',
@@ -13,5 +14,11 @@ export class HomeComponent {
       inline: 'nearest'
     });
   }
+
+  ngOnInit(): void {
+    const image = document.getElementsByClassName('parallax-splitter');
+    new simpleParallax(image);
+  }
+
   constructor() {}
 }
