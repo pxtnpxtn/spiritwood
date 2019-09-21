8dbc281b353f3ea6ce249bfd3a380506140e7a01
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-09-21 16:32:54 -0400

    About us parallax splitter.

--- src/app/modules/about-us/about-us.component.ts
+++ src/app/modules/about-us/about-us.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import simpleParallax from 'simple-parallax-js';
 
 @Component({
   selector: 'app-about-us',
@@ -6,10 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-us.component.scss']
 })
 export class AboutUsComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngOnInit(): void {
+    const image = document.getElementsByClassName('parallax-splitter');
+    new simpleParallax(image);
   }
-
 }
