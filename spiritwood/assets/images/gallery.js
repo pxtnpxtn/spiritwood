9235c66662975e9b2e93f5b8b015932283d21f4d
Paxton Eicher <paxton.w.eicher@gmail.com>
2019-09-23 19:51:26 -0400

    Auto-generated commit

---
+++ spiritwood/assets/images/gallery.js
@@ -0,0 +1,51 @@
+export const gallery1 = [
+  '../../assets/images/gallery/spiritwood-1.jpg',
+  '../../assets/images/gallery/spiritwood-2.jpg',
+  '../../assets/images/gallery/spiritwood-15.jpg',
+  '../../assets/images/gallery/spiritwood-3.jpg',
+  '../../assets/images/gallery/spiritwood-4.jpg',
+  '../../assets/images/gallery/spiritwood-21.jpg',
+  '../../assets/images/gallery/spiritwood-6.jpg',
+  '../../assets/images/gallery/spiritwood-7.jpg',
+  '../../assets/images/gallery/spiritwood-9.jpg',
+  '../../assets/images/gallery/spiritwood-10.jpg'
+];
+
+export const gallery2 = [
+  '../../assets/images/gallery/spiritwood-11.jpg',
+  '../../assets/images/gallery/spiritwood-12.jpg',
+  '../../assets/images/gallery/spiritwood-13.jpg',
+  '../../assets/images/gallery/spiritwood-14.jpg',
+  '../../assets/images/gallery/spiritwood-8.jpg',
+  '../../assets/images/gallery/spiritwood-16.jpg',
+  '../../assets/images/gallery/spiritwood-17.jpg',
+  '../../assets/images/gallery/spiritwood-18.jpg',
+  '../../assets/images/gallery/spiritwood-19.jpg',
+  '../../assets/images/gallery/spiritwood-27.jpg'
+];
+
+export const gallery3 = [
+  '../../assets/images/gallery/spiritwood-5.jpg',
+  '../../assets/images/gallery/spiritwood-22.jpg',
+  '../../assets/images/gallery/spiritwood-23.jpg',
+  '../../assets/images/gallery/spiritwood-24.jpg',
+  '../../assets/images/gallery/spiritwood-32.jpg',
+  '../../assets/images/gallery/spiritwood-26.jpg',
+  '../../assets/images/gallery/spiritwood-20.jpg',
+  '../../assets/images/gallery/spiritwood-28.jpg',
+  '../../assets/images/gallery/spiritwood-29.jpg',
+  '../../assets/images/gallery/spiritwood-30.jpg'
+];
+
+export const gallery4 = [
+  '../../assets/images/gallery/spiritwood-31.jpg',
+  '../../assets/images/gallery/spiritwood-25.jpg',
+  '../../assets/images/gallery/spiritwood-33.jpg',
+  '../../assets/images/gallery/spiritwood-34.jpg',
+  '../../assets/images/gallery/spiritwood-35.jpg',
+  '../../assets/images/gallery/spiritwood-36.jpg',
+  '../../assets/images/gallery/spiritwood-37.jpg',
+  '../../assets/images/gallery/spiritwood-38.jpg',
+  '../../assets/images/gallery/spiritwood-39.jpg',
+  '../../assets/images/gallery/spiritwood-40.jpg'
+];
